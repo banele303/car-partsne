cbf2380bf557880d945a3bf6c3d90910e76e6ecc
alex banele <alexsouthflow@gmail.com>
2026-03-09 20:30:59 +0200

    hello there

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,6 +20,7 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
